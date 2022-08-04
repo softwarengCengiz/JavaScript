4cf6c2577f0397f67f0ba9e51a7af4cd98a5c563
softwarengCengiz <107462800+softwarengCengiz@users.noreply.github.com>
2022-08-05 00:19:54 +0300

    3

--- Interview/Question3.js
+++ Interview/Question3.js
@@ -9,15 +9,22 @@ function Save(item){
 }
 
 function SearchIndex(index){
-    for(var i = orders.length; i>=0; i--){
-        if(index == i){
-        console.log(`Sondan ${index}. eleman -> ${orders[i-1]}`);
-        }
-    }   
+    if(orders.length <= maxOrder){
+    for(var i = orders.length; i>=0; i--){      
+            if(index == i){
+             console.log(`Sondan ${index}. eleman -> ${orders[i-1]}`);
+             }    
+         }   
+    }
+    else{
+         console.log("Error");
+    }
 }
 
 Save(1402);
 Save(1330);
 Save(4050);
+Save(5460);
+Save(6500);
+Save(7500);
 SearchIndex(3);
-
